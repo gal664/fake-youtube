97128220cac95670bc3e6b153a550a14d9dcd545
Gal Yaniv <gal.yaniv@timetoknow.co.il>
2018-08-12 20:09:15 +0300

    started working on homepage component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './Navbar/Navbar';
+import Homepage from './Homepage/Homepage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar/>
+        <Homepage/>
       </div>
     );
   }
---
+++ src/components/app/Homepage/Homepage.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./Homepage.css";
+
+class Homepage extends Component {
+  render() {
+    return (
+      <div className="homepage"></div>
+    );
+  }
+}
+
+export default Homepage;
